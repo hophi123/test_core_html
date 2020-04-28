41395513873612bfdd4cafda1a9e1ba04952ce57
unknown <Admin@phinh.domain.vnext.vn>
2020-04-28 16:12:08 +0700

    commit

--- verify.js
+++ verify.js
@@ -1,31 +1,12 @@
-// const input = document.getElementsByName("number");
-// const dom = document.getElementById('btn-verify');
-// const arr = [];
-
-// for (let i = 0; i < input.length; i++) {
-//     input[i].addEventListener('keyup', e => {
-//         if (e.target.value !== '') {
-//             input[i++].focus();
-//             arr.push(e.target.value);
-//             if (arr.length > 10) {
-//                 dom.classList.remove('disable');
-//             }
-//         }
-//     });
-// }
-let allInputs = document.getElementsByTagName('input');
-let index = 0;
-for(i=0;i<allInputs.length;i++) {
-allInputs[i].onkeydown =trackInputandChangeFocus;
-}
-
-function trackInputandChangeFocus() {
-let allInputsArr = Array.from(allInputs); 
-let presentInput = allInputsArr.indexOf(this)
-if(this.value.length == parseInt(this.getAttribute('maxlength'))) {
-      let next;
-      if(presentInput != 2) next = allInputsArr[presentInput+1]
-      else next = allInputsArr[0]
-      next.focus();
+function toUnicode(elmnt, content) {
+    if (content.length == elmnt.maxLength) {
+        let next = elmnt.tabIndex;
+        if (next < document.forms[0].elements.length - 1) {
+            console.log(document.forms[0].elements.length);
+            document.forms[0].elements[next].focus();
+        } else {
+            document.getElementById('my-submit-id').removeAttribute('disabled');
+            document.getElementById('my-submit-id').classList.remove('my-submit-class');
+        }
     }
 }
